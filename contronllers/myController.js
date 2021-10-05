479ff63302e639eb4fd62f90548b925710483a91
Kniro04 <keenaaron.abril@gracia.lasalle.cat>
2021-10-05 19:31:18 +0200

    Update myController.js

--- contronllers/myController.js
+++ contronllers/myController.js
@@ -1,6 +1,25 @@
 //request, response
 module.exports.helloWorld = (req, res) => {
-    const responseObj = {msg: 'Hello world!'}
+    const responseObj = {"alumnos": [
+        "Gerard",
+        "Khadija",
+        "Alex",
+        "Roger",
+        "Xavier",
+        "Eric",
+        "Miquel",
+        "Alba",
+        "Raul",
+        "Marc",
+        "Jordi",
+        "Marc",
+        "Pol",
+        "Jordi",
+        "Alvar",
+        "Dani",
+        "Alejandro"
+      ],
+      "numAlumnos": 17}
     const responseStatus = 200;
     res.status(responseStatus).send(responseObj);
 }
